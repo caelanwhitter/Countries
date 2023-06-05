430aa24285ecfe6fd266134a47476b528006ff68
Caelan Whitter <93725542+caelanwhitter@users.noreply.github.com>
2023-06-05 12:02:41 -0400

    Update index.js

--- countries/src/index.js
+++ countries/src/index.js
@@ -4,8 +4,6 @@ import {
 } from "react-router-dom";
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
 import Countries from './Countries';
 
 const rootElement = document.querySelector("#root");
